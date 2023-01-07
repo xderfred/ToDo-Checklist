4a19fd9c30814b453129e0a17390e1072f814b39
xderfred <78310976+xderfred@users.noreply.github.com>
2023-01-07 23:59:49 +0100

    added basic sql functions to db

--- database.js
+++ database.js
@@ -1,7 +1,6 @@
-// Importing SQLite3 to our project.
 var sqlite3 = require("sqlite3").verbose();
-// Setting up a database for storing data.
 
+// Setting up a database
 const db = new sqlite3.Database("db.sqlite", (err) => {
     if (err) {
       // Cannot open database
@@ -12,7 +11,6 @@ const db = new sqlite3.Database("db.sqlite", (err) => {
     }
   });
 
-
 db.run(`CREATE TABLE IF NOT EXISTS todo(
                                         id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
                                         content TEXT
@@ -21,3 +19,60 @@ db.run(`CREATE TABLE IF NOT EXISTS todo(
                                                 console.log("ERROR DATABASE:", err)
                                             }
                                         });
+
+
+function getTodoList(callback) {
+    db.all("SELECT * FROM todo", (err, res) => {
+        if(err) {
+            console.log("ERROR SELECT:", err)
+            callback({error: true})
+        } else {
+            callback(res);
+        }
+    });
+}
+
+function getTask(taskId, callback) {
+    db.get("SELECT content FROM todo WHERE id=($task)", {
+        $task: taskId
+    }, (err, res) => {
+        if(err) {
+            console.log("ERROR SELECT SINGLE:", err)
+            callback({error: true})
+        } else {
+            callback(res);
+        }
+    });
+}
+
+function addTask(taskContent, callback) {
+    db.run("INSERT INTO todo VALUES (NULL, $task)", {
+        $task: taskContent
+    }, (err) => {
+        if(err) {
+            console.log("ERROR SELECT SINGLE:", err)
+            callback({error: true})
+        } else {
+            callback({error: false});
+        }
+    });
+}
+
+function deleteTask(taskId, callback) {
+    db.run("DELETE FROM todo WHERE id=($task)", {
+        $task: taskId
+    }, (err) => {
+        if(err) {
+            console.log("ERROR SELECT SINGLE:", err)
+            callback({error: true})
+        } else {
+            callback({error: false});
+        }
+    });
+}
+
+
+module.exports = {
+    // Export all functions to use in the server.js
+    getTodoList, getTask, addTask, deleteTask
+};
