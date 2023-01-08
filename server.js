d3a3985bb24ad777201215aae6e5a06a0da81daa
xderfred <78310976+xderfred@users.noreply.github.com>
2023-01-08 11:20:14 +0100

    Added API endpoints for new database functions

--- server.js
+++ server.js
@@ -1,12 +1,156 @@
-var express = require('express');
-var app = express();
-
-var express = require('express');
-var app = express();
-
+const express = require('express');
 const sqlite = require('./database.js')
 
 const PORT = 3000;
+const app = express();
+
+app.use(express.json())
+
+
+// Endpoints for API calls to Database
+app.get("/api/getOpenTodos", (req, res) => {
+    sqlite.getOpenTodos( (tasks) => {
+        if (tasks.error === true) {
+            res.status(500)
+        }
+
+        console.log("tasks", tasks)
+        res.status(200).send(tasks)
+    }) 
+})
+
+app.get("/api/getDoneTodos", (req, res) => {
+    sqlite.getDoneTodos( (tasks) => {
+        if (tasks.error === true) {
+            res.status(500)
+        }
+
+        console.log("tasks", tasks)
+        res.status(200).send(tasks)
+    }) 
+})
+
+app.get("/api/getOpenTask/:id", (req, res) => {
+    const id = req.params.id
+
+    sqlite.getOpenTask(id, (task) => {
+        console.log("task", task)
+        
+        if (task.error === true) {
+            res.status(500)
+        }
+
+        console.log("task", task)
+        res.status(200).json(task) 
+    }) 
+})
+
+app.get("/api/getDoneTask/:id", (req, res) => {
+    const id = req.params.id
+
+    sqlite.getDoneTask(id, (task) => {
+        console.log("task", task)
+        
+        if (task.error === true) {
+            res.status(500)
+        }
+
+        console.log("task", task)
+        res.status(200).json(task) 
+    }) 
+})
+
+app.post("/api/addTask", (req, res) => {
+    const content = req.body.content;
+
+    console.log("content", content);
+    // Add the new task from the post route.
+    sqlite.addTask(content, (result) => {
+        if (result.error === true) {
+            res.status(500)
+        }
+
+        console.log("result", result)
+        res.status(200)   
+    });
+})
+
+app.delete("/api/deleteOpenTask/:id", (req, res) => {
+    const id = req.params.id;
+
+    console.log("id", id)
+    sqlite.deleteOpenTask(id, (result) => {
+        console.log("Result:", result);
+        if (result.error === true) {
+            res.status(500)
+        }
+
+        console.log("result", result)
+        res.status(200)  
+    });
+})
+
+app.delete("/api/deleteDoneTask/:id", (req, res) => {
+    const id = req.params.id;
+
+    console.log("id", id)
+    sqlite.deleteDoneTask(id, (result) => {
+        console.log("Result:", result);
+        if (result.error === true) {
+            res.status(500)
+        }
+
+        console.log("result", result)
+        res.status(200)  
+    });
+})
+
+app.put("/api/updateTask/:id", (req, res) => {
+    const id = req.params.id;
+    const content = req.body.content;
+    console.log("ID: ", id)
+    console.log("content: ", content)
+
+    sqlite.updateTask(id, content, function(result) {
+        console.log("Result:", result);
+        if (result.error === true) {
+            res.status(500)
+        }
+
+        console.log("result", result)
+        res.status(200)  
+    })
+})
+
+app.post("/api/completeTask/:id", (req, res) => {
+    const id = req.params.id;
+
+    console.log("id", id);
+    
+    sqlite.completeTask(id, (result) => {
+        if (result.error === true) {
+            res.status(500)
+        }
+
+        console.log("result", result)
+        res.status(200)   
+    });
+})
+
+app.post("/api/reopenTask/:id", (req, res) => {
+    const id = req.params.id;
+
+    console.log("id", id);
+    
+    sqlite.reopenTask(id, (result) => {
+        if (result.error === true) {
+            res.status(500)
+        }
+
+        console.log("result", result)
+        res.status(200)   
+    });
+})
 
 
 app.listen(PORT);
